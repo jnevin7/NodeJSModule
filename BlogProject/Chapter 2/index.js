9e70ba59c2718e6555f67b1e0b0c683980d93d5a
jnevin7 <144211993+jnevin7@users.noreply.github.com>
2024-04-22 18:57:52 +0200

    Add files via upload

---
+++ BlogProject/Chapter 2/index.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+const path = require("path");
+app.use(express.static("public"));
+app.listen(3000, ()=> {
+    console.log("App listening on port 3000")
+});
+
+app.get("/about", (req, res) => {
+    res.json({
+        name:"James Nevin"
+    })
+});
+app.get("/", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "index.html"))
+});
+app.get("/about", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "about.html"))
+});
+app.get("/contact", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "contact.html"))
+});
