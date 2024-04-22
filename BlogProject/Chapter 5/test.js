6a04089bcf72b0f8ed9c8359acdee9266835c437
jnevin7 <144211993+jnevin7@users.noreply.github.com>
2024-04-22 19:02:11 +0200

    Add files via upload

---
+++ BlogProject/Chapter 5/test.js
@@ -0,0 +1,49 @@
+"use strict";
+
+// const mongoose = require('mongoose')
+// const BlogPost = require('./models/BlogPost')
+// mongoose.connect('mongodb://localhost/my_database', {
+//     useNewUrlParser:
+//         true
+// });
+// BlogPost.create({
+//     title: 'The Mythbuster Guide to Saving Money on Energy Bills',
+//     body: "If you have been here a long time, you might remember when I went on ITV Tonight to dispense a masterclass in saving money on energy bills. Energy-saving is one of my favourite money topics, because once you get past the boring bullet-point lists, a whole new world of thrifty nerdery opens up. You know those bullet-point lists. You start spotting them everything at this time of year. They go like this:"
+// }, (error, blogpost) => {
+//     console.log(error, blogpost)
+// })
+
+const mongoose = require('mongoose');
+const BlogPost = require('./models/BlogPost');
+
+mongoose.connect('mongodb://localhost/my_database', {
+    useNewUrlParser: true
+});
+
+BlogPost.create({
+    title: 'The Mythbuster Guide to Saving Money on Energy Bills',
+    body: 'If you have been here a long time, you might remember when I went on ITV Tonight to dispense a masterclass in saving money on energy bills. Energy-saving is one of my favourite money topics, because once you get past the boring bullet-point lists, a whole new world of thrifty nerdery opens up. You know those bullet-point lists. You start spotting them everything at this time of year. They go like this:'
+})
+    .then(blogpost => {
+        console.log('Blog post created:', blogpost);
+    })
+    .catch(error => {
+        console.error('Error creating blog post:', error);
+    });
+
+BlogPost.find({
+    title: 'The Mythbuster’s Guide to Saving Money on Energy Bills'
+})
+.then(blogpost => {
+    console.log("Blog post found", blogpost);
+})
+    .catch(error => {
+        console.log("Error finding post: ", error);
+    });
+
+// BlogPost.find({
+//     title: /The/
+// }, (error, blogspot) => {
+//     console.log(error, blogspot)
+// });
+
