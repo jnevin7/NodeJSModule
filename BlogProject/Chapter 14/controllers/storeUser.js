a15a651c4f9131fc9b52a4107144eac074bffdd8
jnevin7 <144211993+jnevin7@users.noreply.github.com>
2024-04-24 01:05:04 +0200

    Add files via upload

---
+++ BlogProject/Chapter 14/controllers/storeUser.js
@@ -0,0 +1,17 @@
+const User = require('../models/User.js');
+const path = require('path');
+
+module.exports = async (req, res) => {
+    try {
+        const user = await User.create(req.body);
+        res.redirect('/');
+    } catch (error) {
+        // Handle error
+        const validationErrors = Object.keys(error.errors).map(key =>
+            error.errors[key].message)
+        req.flash('validationErrors', validationErrors)
+        req.flash('data',req.body) 
+        return res.redirect('/auth/register');
+
+    }
+}
