020bad13dce8ef7b5db0c8b2aa1378c6206f48d8
jnevin7 <144211993+jnevin7@users.noreply.github.com>
2024-04-24 01:02:18 +0200

    Add files via upload

---
+++ BlogProject/Chapter 13/controllers/loginUser.js
@@ -0,0 +1,46 @@
+const bcrypt = require('bcrypt')
+const User = require('../models/User')
+module.exports = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
+        const user = await User.findOne({ username: username });
+
+        if (user) {
+            const same = await bcrypt.compare(password, user.password);
+
+            if (same) {
+                // Store user session, will talk about it later
+                req.session.userId = user._id; 
+                res.redirect('/');
+            } else {
+                res.redirect('/auth/login');
+            }
+        } else {
+            res.redirect('/auth/login');
+        }
+    } catch (error) {
+        console.error('Error finding user:', error);
+        res.status(500).send('An error occurred while processing the request');
+    }
+};
+// module.exports = (req, res) => {
+//     const { username, password } = req.body;
+//     User.findOne({ username: username }, (error, user) => {
+//         if (user) {
+//             bcrypt.compare(password, user.password, (error, same) => {
+//                 if (same) { // if passwords match
+//                     // store user session, will talk about it later
+//                     res.redirect('/')
+//                 }
+//                 else {
+//                     res.redirect('/auth/login')
+//                 }
+//             })
+//         }
+//         else {
+//             res.redirect('/auth/login')
+//         }
+//     })
+// }
+
